333e9a0432a779ab30f8bad1c0cdd6c02854054e
BrokenLinc <lincolnanderson@gmail.com>
2013-10-19 16:03:15 -0400

    Localizing bs directives; adding one for nav

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,47 @@
 'use strict';
 
-angular.module('brokenlincApp', ['ngResource', 'ui.bootstrap'])
+angular.module('brokenlinc', [])
+	.directive('navMenu', function($location) {
+		return function(scope, element, attrs) {
+			var links = element.find('li'),
+			onClass = attrs.navMenu || 'active',
+			routePattern,
+			link,
+			url,
+			currentLink,
+			urlMap = {},
+			i;
+
+			if (!$location.$$html5) {
+				routePattern = /^#[^/]*/;
+			}
+
+			for (i = 0; i < links.length; i++) {
+				link = angular.element(links[i]);
+				url = angular.element(link.find('a')).attr('href');
+
+				if ($location.$$html5) {
+					urlMap[url] = link;
+				} else {
+					urlMap[url.replace(routePattern, '')] = link;
+				}
+			}
+
+			scope.$on('$routeChangeStart', function() {
+				var pathLink = urlMap[$location.path()];
+
+				if (pathLink) {
+					if (currentLink) {
+						currentLink.removeClass(onClass);
+					}
+					currentLink = pathLink;
+					currentLink.addClass(onClass);
+				}
+			});
+		};
+	});
+
+angular.module('brokenlincApp', ['ngResource', 'ui', 'ui.bootstrap', 'brokenlinc'])
 	.config(function ($routeProvider) {
 		$routeProvider
 			.when('/', {
--- app/scripts/controllers/pinHelper.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('brokenlincApp')
-	.controller('PinHelperCtrl', function ($scope) {
-	});
